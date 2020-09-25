2a5a48744bc87ef49bba2305e4cd12d4df2b5941
BluLightShow <maxwell.bromberg@gmail.com>
2020-09-24 23:31:41 -0500

    Initial commit. Welcome to FRMCS.

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import 'fontsource-roboto'
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import React from 'react'
+import Box from '@material-ui/core/Box'
+
+export default class Index extends React.Component {
+  render() {
+    return (
+        <h1>Hello world.</h1>
+    )
+  }
+}
